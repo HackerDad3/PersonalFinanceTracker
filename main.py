7a4d0cd01134ed975bc82156c579ff394b850bcc
William Hutter <william.hutter1984@gmail.com>
2024-11-28 19:13:41 +0800

    chore: initial project setup with README

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    print("Welcome to the Personal Finance Tracker!")
+    print("1. Add a transaction")
+    print("2. View transactions")
+    print("3. Exit")
+    # TODO: Add menu functionality
+
+if __name__ == "__main__":
+    main()
